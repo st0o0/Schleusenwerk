832543e0158911785328b176f3c0d90a962b90b0
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-06-02 07:15:55 +0200

    feat: add Serilog logging with configurable sinks

--- src/Schleusenwerk/Program.cs
+++ src/Schleusenwerk/Program.cs
@@ -2,6 +2,7 @@ using Schleusenwerk.Startup;
 using Servus.Core.Application.Startup;
 
 var runner = AppBuilder.Create()
+    .WithSetup<SchleusenwerkLoggingSetup>()
     .WithSetup<SchleusenwerkActorSystemSetup>()
     .WithSetup<SchleusenwerkServicesSetup>()
     .WithSetup<SchleusenwerkApplicationSetup>()
